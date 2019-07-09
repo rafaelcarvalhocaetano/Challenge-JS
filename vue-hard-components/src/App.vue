13d7a06a5f0c2e8218390043f07a7bc857b35eca
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-07-09 20:10:19 -0300

    finish code

--- vue-hard-components/src/App.vue
+++ vue-hard-components/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Post>
+      <h2 slot="header">Teste H2</h2>
+      <template slot="body">
+      <p>Componentes de Slot</p>
+      <span>.....</span>
+      </template>
+      <p  slot="footer">Footer</p>
+    </Post>
+    <Post titulo="Via Props"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Post from './components/Post'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Post
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-hard-components/src/components/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <slot name="header"></slot>
+    </div>
+    <div class="post-body">
+      <slot name="body"></slot>
+    </div>
+    <div class="post-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .post {
+    margin: 15px auto;
+  }
+  .post-header {
+    padding: 5px 12px;
+    background-color: #eee;
+  }
+  .post-body {
+    font-size: 16px;
+  }
+  .post-footer {
+    font-style: italic;
+  }
+</style>
