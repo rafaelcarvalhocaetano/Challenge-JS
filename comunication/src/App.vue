230c2a0f192419ac10893f9ebc886b79607a2b20
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-07-09 13:25:42 -0300

    Finish code

---
+++ comunication/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="jumbotron junbotron-fluid">
+      <div class="container">
+        <div class="display-4">Lista de Filmes</div>
+        <p class="lead">Treinando comunicação entre componentes no VUE</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <FilmesLista />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import FilmesLista from './components/FilmesLista';
+
+export default {
+  components: {
+    FilmesLista
+  }
+};
+</script>
